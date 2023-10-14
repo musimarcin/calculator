083ea84cfcc16d6f1e152e632af5601a4df4a143
musimarcin <marcinm994@gmail.com>
2023-10-14 21:05:31 +0200

    Added functionality to some buttons.

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-const button_cont = document.querySelector("#buttons");
+const operand = document.querySelectorAll(".number");
+const current = document.querySelector("#current");
+const all_clear = document.querySelector("#AC");
+const clear = document.querySelector("#C");
+const operation = document.querySelectorAll(".operation");
 
 
-for (let i = 19; i >= 0; i--) {
-    const button = document.createElement('div');
-    button_cont.appendChild(button);
-    button.setAttribute('class', "operand");
-    i < 16 ? button.textContent = `${i}` : "";
 
-    console.log(`${i}`);
+all_clear.addEventListener("click", () => clearContent());
+operand.forEach(e => e.addEventListener("click", () => addElement(e)));
+clear.addEventListener("click", () => deleteLast());
+operand.forEach(e => e.addEventListener("click", () => setOperation(e.textContent)));
+
+
+
+function add(a,b) {
+    return a+b;
+}
+
+function minus(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function addElement(element) {
+    let current_array = Array.from(current.textContent);
+    const point = ".";
+    if (element.textContent == point) {
+        if (!current_array.some((x) => x === point)) {
+            current.textContent += element.textContent;
+        }
+    } else {
+        current.textContent += element.textContent;
+    }
+}
+
+function clearContent() {
+    current.textContent = "";
+}
+
+function deleteLast() {
+    current.textContent = current.textContent.toString().slice(0,-1);
+}
+
+function setOperation(operation) {
+    return operation;
 }
